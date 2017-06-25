404481192b76cbd3cf5ea3eb4ce6750cf2234517
Michael Lilley <michael.lilley@gmail.com>
2017-06-25 10:21:23 -0700

    Add license, adjust for hex package

--- mix.exs
+++ mix.exs
@@ -4,11 +4,13 @@ defmodule CertCache.Mixfile do
   def project do
     [
       app: :cert_cache,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.4",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "A certificate cache for Elixir / Pheonix",
+      package: package()
     ]
   end
 
@@ -21,6 +23,14 @@ defmodule CertCache.Mixfile do
   defp deps do
     [
       {:credo, "~> 0.8", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Michael Lilley"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/mlilley/cert_cache"}
     ]
   end
 end
